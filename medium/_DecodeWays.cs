8b3f32a0116ddf5b5630bf4f8c334475a6a496cb
[buse duran] <buseduran500@gmail.com>
2023-07-27 21:08:28 +0300

    1m

--- medium/_DecodeWays.cs
+++ medium/_DecodeWays.cs
@@ -4,6 +4,33 @@ public class _DecodeWays
 {
     public int NumDecodings(string s)
     {
-        return 0;
+        int n = s.Length;
+
+        // Temel durum: boş bir dizge 1 şekilde çözümlenir.
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        // Eğer ilk karakter '0' ise, çözümlenemez. Bu nedenle dp[1] 0 olacak,
+        // aksi halde 1 olacak.
+        dp[1] = s[0] == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            // Son rakam tek başına çözümlenebilir (yani '0' değilse),
+            // dp[i - 1]'in değerini dp[i]'ye ekliyoruz.
+            if (1 <= int.Parse(s[i - 1].ToString()) && int.Parse(s[i - 1].ToString()) <= 9)
+            {
+                dp[i] += dp[i - 1];
+            }
+
+            // Son iki rakamın geçerli bir çözüm oluşturup oluşturmadığını kontrol ediyoruz.
+            int twoDigit = int.Parse(s.Substring(i - 2, 2));
+            if (10 <= twoDigit && twoDigit <= 26)
+            {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
     }
 }
